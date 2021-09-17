70dfa84351b31c7354b7f55c631e25df452c772a
fapulito <36351826+fapulito@users.noreply.github.com>
2021-09-17 16:34:04 -0700

    Change Site Title to Moajr Beta Test
    Adjusting the site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Moajr Beta Test`,
+    description: `Leonard is helping with DevOps here - This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -21,8 +21,9 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#C0C2C9`,  // was `#663399`
+
+        theme_color: `#014421`, // `#663399`
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
